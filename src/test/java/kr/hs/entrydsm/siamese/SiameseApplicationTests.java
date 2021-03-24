3f2fce3fa168486705447a39151313081821270d
JeongWooYeong <wjd030811@gmail.com>
2021-03-24 23:24:54 +0900

    [ADD] 프로젝트 기본 구조 설정

---
+++ src/test/java/kr/hs/entrydsm/siamese/SiameseApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.entrydsm.siamese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiameseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

5fa2f7224928eb673db2c72b68e55f46f3ac1762
JeongWooYeong <wjd030811@gmail.com>
2021-04-01 23:41:47 +0900

    [ADD] 이미지 컨트롤러

--- src/main/java/kr/hs/entrydsm/admin/controller/ImageController.java
+++ src/main/java/kr/hs/entrydsm/admin/controller/ImageController.java
@@ -2,23 +2,24 @@ package kr.hs.entrydsm.admin.controller;
 
 import kr.hs.entrydsm.admin.service.ImageService;
 import kr.hs.entrydsm.admin.service.dto.ImageResponse;
+import kr.hs.entrydsm.common.security.JWTRequired;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
+import java.io.IOException;
+import java.util.Optional;
 
 @RequiredArgsConstructor
+@JWTRequired
 @RestController
 @RequestMapping("/image")
 public class ImageController {
     private final ImageService imageService;
 
     @PostMapping
-    public ImageResponse createImage(@Valid @RequestParam("file") MultipartFile file) {
-        return imageService.createImage(file);
+    public ImageResponse createImage(@Nullable @RequestParam MultipartFile file) throws IOException {
+        return imageService.createImage(Optional.ofNullable(file));
     }
 }

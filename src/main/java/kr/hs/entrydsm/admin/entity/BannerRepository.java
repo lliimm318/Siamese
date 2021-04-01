9bb2ef429680a1d5b5b78be284ff97c5eb4f68ee
JeongWooYeong <wjd030811@gmail.com>
2021-04-01 23:36:39 +0900

    [ADD] 배너 엔티티

--- src/main/java/kr/hs/entrydsm/admin/entity/BannerRepository.java
+++ src/main/java/kr/hs/entrydsm/admin/entity/BannerRepository.java
@@ -1,5 +1,8 @@
 package kr.hs.entrydsm.admin.entity;
 
-public interface BannerRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface BannerRepository extends CrudRepository<Banner, Long> {
 }

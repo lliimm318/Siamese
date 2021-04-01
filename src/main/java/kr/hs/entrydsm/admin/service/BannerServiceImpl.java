4ce86e10ba4f05a1752ced6c242075dbf4a82f62
JeongWooYeong <wjd030811@gmail.com>
2021-04-01 23:40:25 +0900

    [ADD] 배너 서비스

--- src/main/java/kr/hs/entrydsm/admin/service/BannerServiceImpl.java
+++ src/main/java/kr/hs/entrydsm/admin/service/BannerServiceImpl.java
@@ -1,19 +1,39 @@
 package kr.hs.entrydsm.admin.service;
 
+import kr.hs.entrydsm.admin.entity.*;
 import kr.hs.entrydsm.admin.service.dto.BannerRequest;
+import kr.hs.entrydsm.admin.service.exception.BannerExistException;
+import kr.hs.entrydsm.admin.service.exception.BannerNotFoundException;
+import kr.hs.entrydsm.admin.service.exception.ForbiddenException;
+import kr.hs.entrydsm.admin.service.exception.ImageNotFoundException;
+import kr.hs.entrydsm.common.security.AuthMiddleware;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class BannerServiceImpl implements BannerService {
+    private final ImageRepository imageRepository;
+    private final BannerRepository bannerRepository;
+
     @Override
     public void createBanner(BannerRequest bannerRequest) {
-
+        Image image = imageRepository.findById(bannerRequest.getImageId()).orElseThrow(ImageNotFoundException::new);
+        if (bannerRepository.findById(image.getId()).isPresent()) { throw new BannerExistException(); }
+        Club currentClub = AuthMiddleware.currentClub();
+        if (!currentClub.getId().equals(image.getClub().getId())) { throw new ForbiddenException(); }
+        bannerRepository.save(Banner.builder()
+                .imageId(image.getId())
+                .ttl(bannerRequest.getTtl())
+                .build());
     }
 
     @Override
-    public void deleteBanner(int imageId) {
-
+    public void deleteBanner(long imageId) {
+        Banner banner = bannerRepository.findById(imageId).orElseThrow(BannerNotFoundException::new);
+        Image image = imageRepository.findById(imageId).orElseThrow(ImageNotFoundException::new);
+        Club currentClub = AuthMiddleware.currentClub();
+        if (!image.getClub().getId().equals(currentClub.getId())) { throw new ForbiddenException(); }
+        bannerRepository.delete(banner);
     }
 }

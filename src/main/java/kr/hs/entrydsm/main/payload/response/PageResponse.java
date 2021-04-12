a0b94f9679d28299ad8431dcad351e62fe572080
임서영 <sylim0318@naver.com>
2021-04-12 13:06:33 +0900

    [ADD] 페이지 디티오

---
+++ src/main/java/kr/hs/entrydsm/main/payload/response/PageResponse.java
@@ -0,0 +1,17 @@
+package kr.hs.entrydsm.main.payload.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+public class PageResponse {
+    private Integer totalPosts;
+    private Integer totalPage;
+    private List<PostResponse> bestListResponse;
+
+}

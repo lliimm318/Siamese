b75cd594c611925b1af2de33929d04d7bd14c8ee
JeongWooYeong <wjd030811@gmail.com>
2021-04-14 14:55:11 +0900

    [ADD] 동아리 인증 EP

--- src/main/java/kr/hs/entrydsm/admin/service/ClubService.java
+++ src/main/java/kr/hs/entrydsm/admin/service/ClubService.java
@@ -1,5 +1,6 @@
 package kr.hs.entrydsm.admin.service;
 
+import kr.hs.entrydsm.admin.service.dto.ClubResponse;
 import kr.hs.entrydsm.admin.service.dto.LoginRequest;
 import kr.hs.entrydsm.admin.service.dto.LoginResponse;
 import kr.hs.entrydsm.admin.service.dto.RefreshResponse;
@@ -7,4 +8,5 @@ import kr.hs.entrydsm.admin.service.dto.RefreshResponse;
 public interface ClubService {
     LoginResponse login(LoginRequest loginRequest);
     RefreshResponse refresh();
+    ClubResponse getClub();
 }

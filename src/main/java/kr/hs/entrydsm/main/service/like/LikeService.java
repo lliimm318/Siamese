e2fbb3ad38e8af31b1ef2ad6cad0ff3c4d141df0
임서영 <sylim0318@naver.com>
2021-05-31 19:58:46 +0900

    [UPDATE] 좋아요 서비스

--- src/main/java/kr/hs/entrydsm/main/service/like/LikeService.java
+++ src/main/java/kr/hs/entrydsm/main/service/like/LikeService.java
@@ -1,8 +1,7 @@
 package kr.hs.entrydsm.main.service.like;
 
 import kr.hs.entrydsm.main.payload.response.PageResponse;
-
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 
 public interface LikeService {
     void createLike(Integer postId);

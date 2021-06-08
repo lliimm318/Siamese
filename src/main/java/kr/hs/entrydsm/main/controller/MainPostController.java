0b5075ef999bab874055bb1d23d8fbbe29ccee9b
임서영 <66578746+lliimm318@users.noreply.github.com>
2021-06-08 16:48:50 +0900

    Update MainPostController.java

--- src/main/java/kr/hs/entrydsm/main/controller/MainPostController.java
+++ src/main/java/kr/hs/entrydsm/main/controller/MainPostController.java
@@ -42,4 +42,9 @@ public class MainPostController {
         return likeService.bestPostList(page);
     }
 
+    @GetMapping("/test")
+    public String hello() {
+        return "HELL";
+    }
+    
 }

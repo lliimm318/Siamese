edb88f1054e8b9ef01211c020a74e4bcbf30d6a7
임서영 <sylim0318@naver.com>
2021-06-29 07:25:01 +0900

    [DELETE] 겹치는 익셉션 삭제

--- src/main/java/kr/hs/entrydsm/admin/service/exception/ForbiddenException.java
+++
@@ -1,8 +0,0 @@
-package kr.hs.entrydsm.admin.service.exception;
-
-import kr.hs.entrydsm.common.exception.ServiceException;
-import org.springframework.http.HttpStatus;
-
-public class ForbiddenException extends ServiceException {
-    public ForbiddenException() { super(HttpStatus.FORBIDDEN.value(), "Forbidden (Invalid access)"); }
-}

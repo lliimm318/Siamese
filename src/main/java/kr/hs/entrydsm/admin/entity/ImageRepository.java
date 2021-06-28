942234f8560105e114f90aea3750202ae85e642d
임서영 <sylim0318@naver.com>
2021-06-29 07:21:24 +0900

    [DELETE] 겹치는 레포 삭제

--- src/main/java/kr/hs/entrydsm/admin/entity/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.hs.entrydsm.admin.entity;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("adminImageRepository")
-public interface ImageRepository extends CrudRepository<Image, Long> {
-
-}

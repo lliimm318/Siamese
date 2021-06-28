e831c640e996b9330c1d3f21128d73c21958ca10
임서영 <sylim0318@naver.com>
2021-06-29 07:25:54 +0900

    [DELETE] 겹치는 게시글 서비스 삭제

--- src/main/java/kr/hs/entrydsm/admin/service/PostService.java
+++
@@ -1,9 +0,0 @@
-package kr.hs.entrydsm.admin.service;
-
-import kr.hs.entrydsm.admin.service.dto.PostRequest;
-
-public interface PostService {
-    void createPost(PostRequest postRequest);
-    void deletePost(long postId);
-    void updatePost(long postId, PostRequest postRequest);
-}

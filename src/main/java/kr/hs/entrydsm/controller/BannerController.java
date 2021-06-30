21d8f7e74a5be79b93ab0ea891e50e0462b7b7a1
임서영 <sylim0318@naver.com>
2021-06-30 21:56:22 +0900

    [FIX] 베너 컨트롤러 충돌 오류 해결

--- src/main/java/kr/hs/entrydsm/controller/BannerController.java
+++ src/main/java/kr/hs/entrydsm/controller/BannerController.java
@@ -1,9 +1,9 @@
 package kr.hs.entrydsm.controller;
 
-import kr.hs.entrydsm.service.banner.BannerService;
 import kr.hs.entrydsm.payload.request.BannerRequest;
 import kr.hs.entrydsm.payload.response.BannerResponse;
 import kr.hs.entrydsm.security.JWTRequired;
+import kr.hs.entrydsm.service.banner.BannerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +17,6 @@ import java.util.List;
 public class BannerController {
 
     private final BannerService bannerService;
-
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/banner")

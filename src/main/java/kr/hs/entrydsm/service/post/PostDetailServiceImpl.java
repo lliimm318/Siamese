3a7f6679db2a660b0e1bb40023513c3222a41e0d
임서영 <sylim0318@naver.com>
2021-06-30 20:24:59 +0900

    [ADD] 게시글 상세보기 서비스

--- src/main/java/kr/hs/entrydsm/service/post/PostDetailServiceImpl.java
+++ src/main/java/kr/hs/entrydsm/service/post/PostDetailServiceImpl.java
@@ -1,8 +1,9 @@
-package kr.hs.entrydsm.main.service.post;
+package kr.hs.entrydsm.service.post;
 
-import kr.hs.entrydsm.main.enitity.Post;
-import kr.hs.entrydsm.main.enitity.repository.PostRepository;
-import kr.hs.entrydsm.main.payload.response.PostDetailResponse;
+import kr.hs.entrydsm.enitity.Post;
+import kr.hs.entrydsm.enitity.repository.PostRepository;
+import kr.hs.entrydsm.payload.response.PostDetailResponse;
+import kr.hs.entrydsm.service.exception.PostNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,18 +13,17 @@ public class PostDetailServiceImpl implements PostDetailService {
     private final PostRepository postRepository;
 
     @Override
-    public PostDetailResponse getDetailPost(Integer id) {
+    public PostDetailResponse getDetailPost(long id) {
         Post post = postRepository.findById(id)
-                .orElseThrow(RuntimeException::new);
+                .orElseThrow(PostNotFoundException::new);
 
         return PostDetailResponse.builder()
                     .id(post.getId())
                     .title(post.getTitle())
                     .author(post.getAuthor())
-                    .description(post.getDescription())
                     .content(post.getContent())
-                    .image(post.getImage().getPath())
-                    .date(post.getDate())
+                    .image(post.getImageId())
+                    .date(post.getCreatedAt())
                     .build();
     }
 }

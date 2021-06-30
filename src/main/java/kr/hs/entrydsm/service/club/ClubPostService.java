ff51b5456d81101534667e3756515077a03cc74d
임서영 <sylim0318@naver.com>
2021-06-30 20:22:09 +0900

    [ADD] 동아리 게시글 서비스

--- src/main/java/kr/hs/entrydsm/service/club/ClubPostService.java
+++ src/main/java/kr/hs/entrydsm/service/club/ClubPostService.java
@@ -1,13 +1,19 @@
-package kr.hs.entrydsm.admin.service;
+package kr.hs.entrydsm.service.club;
 
-import kr.hs.entrydsm.admin.service.dto.PostRequest;
-import kr.hs.entrydsm.admin.service.dto.PostResponse;
-import kr.hs.entrydsm.admin.service.dto.PostsResponse;
+import kr.hs.entrydsm.payload.request.PostRequest;
+import kr.hs.entrydsm.payload.response.PostResponse;
+import kr.hs.entrydsm.payload.response.PostsResponse;
 
 public interface ClubPostService {
+
     void createPost(PostRequest postRequest);
+
     void deletePost(long postId);
+
     void updatePost(long postId, PostRequest postRequest);
+
     PostsResponse getPosts();
+
     PostResponse getPost(long postId);
+
 }

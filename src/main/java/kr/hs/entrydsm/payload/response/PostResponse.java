94e47755a9f9615b48763b8de3ef77759a020b45
임서영 <sylim0318@naver.com>
2021-06-30 20:13:22 +0900

    [ADD] 게시글 디티오

--- src/main/java/kr/hs/entrydsm/payload/response/PostResponse.java
+++ src/main/java/kr/hs/entrydsm/payload/response/PostResponse.java
@@ -1,11 +1,17 @@
-package kr.hs.entrydsm.admin.service.dto;
+package kr.hs.entrydsm.payload.response;
 
+import kr.hs.entrydsm.enitity.PostType;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class PostResponse {
+
+    private long id;
+
+    private long image;
+
     private String title;
 
     private String author;

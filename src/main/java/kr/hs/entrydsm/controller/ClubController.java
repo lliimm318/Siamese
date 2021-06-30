99c82dad9ee2d0b0cac8c1543dd2eaef34ee77a6
임서영 <sylim0318@naver.com>
2021-06-30 19:57:32 +0900

    [ADD] 동아리 컨트롤러

--- src/main/java/kr/hs/entrydsm/controller/ClubController.java
+++ src/main/java/kr/hs/entrydsm/controller/ClubController.java
@@ -1,13 +1,14 @@
-package kr.hs.entrydsm.admin.controller;
+package kr.hs.entrydsm.controller;
 
-import kr.hs.entrydsm.admin.service.ClubService;
-import kr.hs.entrydsm.admin.service.dto.ClubResponse;
-import kr.hs.entrydsm.admin.service.dto.LoginRequest;
-import kr.hs.entrydsm.admin.service.dto.LoginResponse;
-import kr.hs.entrydsm.admin.service.dto.RefreshResponse;
-import kr.hs.entrydsm.common.security.JWTRequired;
-import kr.hs.entrydsm.common.security.RefreshRequired;
+import kr.hs.entrydsm.service.club.ClubService;
+import kr.hs.entrydsm.payload.response.ClubResponse;
+import kr.hs.entrydsm.payload.request.LoginRequest;
+import kr.hs.entrydsm.payload.response.LoginResponse;
+import kr.hs.entrydsm.payload.response.RefreshResponse;
+import kr.hs.entrydsm.security.JWTRequired;
+import kr.hs.entrydsm.security.RefreshRequired;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -17,9 +18,11 @@ import javax.validation.Valid;
 @RequestMapping("/club")
 public class ClubController {
     private final ClubService clubService;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
     public LoginResponse login(@Valid @RequestBody LoginRequest loginRequest) {
+        System.out.println(passwordEncoder.encode("qwe123"));
         return clubService.login(loginRequest);
     }
 

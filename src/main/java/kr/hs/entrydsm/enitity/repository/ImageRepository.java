438ad1090a9ca08814cad964a251921b3489b9da
임서영 <sylim0318@naver.com>
2021-06-30 19:59:47 +0900

    [ADD] 이미지 레포지토리

--- src/main/java/kr/hs/entrydsm/enitity/repository/ImageRepository.java
+++ src/main/java/kr/hs/entrydsm/enitity/repository/ImageRepository.java
@@ -1,6 +1,6 @@
-package kr.hs.entrydsm.main.enitity.repository;
+package kr.hs.entrydsm.enitity.repository;
 
-import kr.hs.entrydsm.main.enitity.Image;
+import kr.hs.entrydsm.enitity.Image;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 

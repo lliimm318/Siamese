0282a47d3dde6db1dbbc89ea460e0a606deebd6f
임서영 <sylim0318@naver.com>
2021-07-05 16:10:16 +0900

    [FIX] 게시글 필요 없는 부분 삭제

--- src/main/java/kr/hs/entrydsm/service/post/PostService.java
+++ src/main/java/kr/hs/entrydsm/service/post/PostService.java
@@ -1,11 +1,11 @@
 package kr.hs.entrydsm.service.post;
 
-import kr.hs.entrydsm.payload.response.PostResponse;
+import kr.hs.entrydsm.payload.response.MainPostResponse;
 
 import java.util.List;
 
 public interface PostService {
 
-    List<PostResponse> getListPost();
+    List<MainPostResponse> getListPost();
 
 }

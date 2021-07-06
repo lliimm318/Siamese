b4e48fd186cb14185e9b1867cbcd871cfdb7e923
임서영 <sylim0318@naver.com>
2021-07-06 11:01:40 +0900

    [FIX] 임포트 변경

--- src/main/java/kr/hs/entrydsm/service/like/LikeService.java
+++ src/main/java/kr/hs/entrydsm/service/like/LikeService.java
@@ -1,12 +1,12 @@
 package kr.hs.entrydsm.service.like;
 
-import kr.hs.entrydsm.payload.response.PageResponse;
+import kr.hs.entrydsm.payload.response.RecommendPageResponse;
 import org.springframework.data.domain.Pageable;
 
 public interface LikeService {
 
     void createLike(long postId);
 
-    PageResponse bestPostList(Pageable page);
+    RecommendPageResponse bestPostList(Pageable page);
 
 }

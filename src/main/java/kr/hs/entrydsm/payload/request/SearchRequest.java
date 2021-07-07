4447476b054906d48dd26ee6be0301c3e99b7abf
임서영 <sylim0318@naver.com>
2021-07-07 11:10:36 +0900

    [ADD] 게시글 검색 디티오 추가

---
+++ src/main/java/kr/hs/entrydsm/payload/request/SearchRequest.java
@@ -0,0 +1,12 @@
+package kr.hs.entrydsm.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SearchRequest {
+
+    private String searchWord;
+
+}

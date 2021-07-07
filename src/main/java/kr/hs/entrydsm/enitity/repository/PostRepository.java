bfcefdcabbbb93908cca30944eed34b323721850
임서영 <sylim0318@naver.com>
2021-07-07 11:09:40 +0900

    [UPDATE] 게시글 검색 추가

--- src/main/java/kr/hs/entrydsm/enitity/repository/PostRepository.java
+++ src/main/java/kr/hs/entrydsm/enitity/repository/PostRepository.java
@@ -1,19 +1,25 @@
 package kr.hs.entrydsm.enitity.repository;
 
+import kr.hs.entrydsm.enitity.Club;
 import kr.hs.entrydsm.enitity.Post;
+import kr.hs.entrydsm.enitity.PostType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository("mainRepository")
+@Repository
 public interface PostRepository extends CrudRepository<Post, Long> {
 
     List<Post> findAllBy();
 
-    Page<Post> findByOrderByLikesDesc(Pageable pageable);
+    List<Post> findByOrderByLikesDesc();
 
+    List<Post> findAllByTitleContainsOrContentContainsOrAuthorContainsOrClubNameContainsOrderByCreatedAtDesc
+            (String title, String content, String author, String clubName);
 }
 
